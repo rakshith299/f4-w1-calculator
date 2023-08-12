c6af34d9e9bc04103c6a412bc3e5fdc18d56af51
rakshith299 <rockyrocky14789@gmail.com>
2023-08-12 12:14:52 +0530

    react calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import Calculator from "./Components/Calculator.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => {
+
+  return(
+    <div className="card">
+      <h1>React Calculator</h1>
+      <Calculator/>
     </div>
-  );
+  )
+  
 }
 
 export default App;
